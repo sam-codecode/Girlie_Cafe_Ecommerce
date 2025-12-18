c7d2e0bafc615c5ab6e57f51a90c987a3590aceb
Anunitha <anunitha01@gmail.com>
2025-12-18 22:50:04 +0800

    ProductDAO : Implement product creation and listing

--- src/dao/dao/ProductDAO.java
+++ src/dao/dao/ProductDAO.java
@@ -1,5 +1,69 @@
 package dao;
 
+import database.DBConnection;
+import model.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDAO {
+
+    // Create (Add New Product)
+
+    public boolean addProduct(Product product) {
+
+        String sql = "INSERT INTO products (category_id, name, description, price, stock, image_name) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, product.getCategoryId());
+            ps.setString(2, product.getName());
+            ps.setString(3, product.getDescription());
+            ps.setDouble(4, product.getPrice());
+            ps.setInt(5, product.getStock());
+            ps.setString(6, product.getImageName());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // Read (Get All Products)
     
+    public List<Product> getAllProducts() {
+
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM products";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Product product = new Product();
+
+                product.setProductId(rs.getInt("product_id"));
+                product.setCategoryId(rs.getInt("category_id"));
+                product.setName(rs.getString("name"));
+                product.setDescription(rs.getString("description"));
+                product.setPrice(rs.getDouble("price"));
+                product.setStock(rs.getInt("stock"));
+                product.setImageName(rs.getString("image_name"));
+
+                products.add(product);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return products;
+    }
 }
