9144c21fdc96010664e1827e7cc2b86af13e6fd0
Anunitha <anunitha01@gmail.com>
2025-12-21 08:47:31 +0800

    CategoryDAO : Implement category creation and listing

--- src/dao/dao/CategoryDAO.java
+++ src/dao/dao/CategoryDAO.java
@@ -1,5 +1,65 @@
 package dao;
 
+import database.DBConnection;
+import model.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDAO {
-    
+
+    // ===========================
+    // Create (Add New Category)
+    // ===========================
+    public boolean addCategory(Category category) {
+
+        String sql = "INSERT INTO categories (category_name, description, image_name) VALUES (?, ?, ?)";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, category.getCategoryName());
+            ps.setString(2, category.getDescription());
+            ps.setString(3, category.getImageName());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // =========================
+    // Read (Get All Category)
+    // =========================
+    public List<Category> getAllCategories() {
+
+        List<Category> categories = new ArrayList<>();
+        String sql = "SELECT * FROM categories";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Category category = new Category();
+
+                category.setCategoryId(rs.getInt("category_id"));
+                category.setCategoryName(rs.getString("category_name"));
+                category.setDescription(rs.getString("description"));
+                category.setImageName(rs.getString("image_name"));
+
+                categories.add(category);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return categories;
+    }
 }
